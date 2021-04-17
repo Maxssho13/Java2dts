a45ed182dae7bd3e3fdf6f96f4825c7bdb7ff8af
Max Glass <glassmax13@gmail.com>
2021-04-17 18:43:45 -0400

    Fix parser crashing to invalid setter param names
    Kotlin setters can have weird names sometimes so let's just hack it

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ if (!existsSync(process.argv?.[2])) {
 
 let intext = readFileSync(process.argv[2], "utf8");
 
+// TODO: For kotlin, the decompilation will name the setter arguments "<set-?>" which is an invalid name(not to mention very helpful)
+// so for now let's just replace it with "arg0" and eventually can derive an algorithm to give them apt names.
+intext = intext.replace(/<set-\?>/g, "arg0");
+
 const cst = parse(intext);
 
 let out = visitor.visit(cst);
